adf36ce71e6b36d415b94a427e635a0d332acb00
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-10-27 17:45:57 +0100

    Migrating to the bent package

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: "./env" });
 
 import morgan from "morgan";
 import express from "express";
@@ -17,17 +17,6 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 // This is where we mount the routes
 app.use("/api/v1/", bibleVerseRouter);
-
-// leave this for later
-// async function getVerse(): Promise<any> {
-//   try {
-//     const url: string = "https://wol.jw.org/en/wol/dx/r1/lp-e/1001070105/2";
-//     const html = await fetch(url).then((data) => data.text());
-//     console.log(html);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
 
 // exporting express app so it can be used by other modules
 export default app;
--- src/backend/controllers/bibleVerseController.ts
+++ src/backend/controllers/bibleVerseController.ts
@@ -1,6 +1,8 @@
-import request from 'request';
+import bent from "bent";
+
 import { parse } from "node-html-parser";
 import { Request, Response } from "express";
+import console from "console";
 
 async function getVerse(
   req: Request,
@@ -8,47 +10,47 @@ async function getVerse(
   next: Function
 ): Promise<void> {
   try {
-    request(
-      `https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/${req.params.bookNumber}/1#study=discover&v=${req.params.bookNumber}:${req.params.chapter}:${req.params.verse}`,
-      (err: Error, response: any, body: string) => {
-        if (err) {
-          console.error(err);
-          res.status(500).json({ error: "Server-side error, aborted request." });
+    const getString = await bent("string");
+    // `https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/${req.params.bookNumber}/1#study=discover&v=${req.params.bookNumber}:${req.params.chapter}:${req.params.verse}`
 
-          next(err);
-          return;
-        }
+    const responseString = await getString(
+      `https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/${Number(
+        req.params.bookNumber
+      )}/1#study=discover&v=${req.params.bookNumber}:${req.params.chapter}:${
+        req.params.verse
+      }`
+    );
 
-        const data = parse(body);
-        const verseNumber = parseInt(req.params.verse);
-        const idString = `p${verseNumber + 1}`;
+    const data = parse(responseString);
+    const verseNumber = parseInt(req.params.verse);
+    const idString = `p${verseNumber + 1}`;
 
-        const verse = data
-          // getting all the elements with the "p" tag
-          .getElementsByTagName("p")
-          .filter(
-            // matching the id to the right element
-            (val) => val.id === idString && val.rawAttrs.includes(`class="sb"`)
-          )
-          // Map returns an array
-          .map((val) => {
-            // Remove non-alpha-whitespace characters e.g. <p>hello</p> => phellop
-            const outputStr = val.text.replace(/[^a-zA-Z-.\s]/g, "").trim();
-            return outputStr;
-          });
+    const verse = data
+      // getting all the elements with the "p" tag
+      .getElementsByTagName("p")
+      .filter(
+        // matching the id to the right element
+        (val) => val.id === idString && val.rawAttrs.includes(`class="sb"`)
+      )
+      // Map returns an array
+      .map((val) => {
+        // Remove non-alpha-whitespace characters e.g. <p>hello</p> => phellop
+        const outputStr = val.text.replace(/[^a-zA-Z-.\s]/g, "").trim();
+        return outputStr;
+      });
 
-        // sending response to the user
-        const resultVerse = verse[0];
+    // sending response to the user
+    const resultVerse = verse[0];
 
-        if (!resultVerse) {
-          res.status(400).json({ error: "Invalid entry, please check your request and retry." });
-          next(Error('Result is undefined!'));
-          return;
-        }
+    if (!resultVerse) {
+      res.status(400).json({
+        error: "Invalid entry, please check your request and retry.",
+      });
+      next(Error("Result is undefined!"));
+      return;
+    }
 
-        res.status(200).json({ data: `${resultVerse}` });
-      }
-    )
+    res.status(200).json({ data: `${resultVerse}` });
   } catch (err) {
     console.error(err);
     next(err);
