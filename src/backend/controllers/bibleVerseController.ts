fbec29f6ce476eacc784986e2920074902c35e84
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-10-30 10:36:19 +0000

    Replacing axios with bent and fixing functionality

--- src/backend/controllers/bibleVerseController.ts
+++ src/backend/controllers/bibleVerseController.ts
@@ -14,36 +14,23 @@ async function getVerse(
     // `https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/${req.params.bookNumber}/1#study=discover&v=${req.params.bookNumber}:${req.params.chapter}:${req.params.verse}`
 
     const responseString = await getString(
-      `https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/${Number(
-        req.params.bookNumber
-      )}/1#study=discover&v=${req.params.bookNumber}:${req.params.chapter}:${
-        req.params.verse
-      }`
+      `https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/${req.params.bookNumber}/${req.params.chapter}#study=discover&v=${req.params.bookNumber}:${req.params.chapter}:${req.params.verse}`
     );
 
     const data = parse(responseString);
-    const verseNumber = parseInt(req.params.verse);
-    const idString = `p${verseNumber + 1}`;
+    const idString: string = `v${req.params.bookNumber}-${req.params.chapter}-${req.params.verse}-1`;
+    console.log(idString);
 
     const verse = data
-      // getting all the elements with the "p" tag
-      .getElementsByTagName("p")
-      .filter(
-        // matching the id to the right element
-        (val) => val.id === idString && val.rawAttrs.includes(`class="sb"`)
-      )
-      // Map returns an array
-      .map((val) => {
-        // Remove non-alpha-whitespace characters e.g. <p>hello</p> => phellop
-        const outputStr = val.text.replace(/[^a-zA-Z-.\s]/g, "").trim();
-        return outputStr;
-      });
+      .getElementById(idString)
+      // Remove non-alpha-whitespace characters e.g. <p>hello</p> => phellop
+      .text.replace(/[0-9+*]/g, "")
+      .trim();
 
     // sending response to the user
-    const resultVerse = verse[0];
     console.log(verse);
 
-    if (!resultVerse) {
+    if (!verse) {
       res.status(400).json({
         error: "Invalid entry, please check your request and retry.",
       });
@@ -51,7 +38,7 @@ async function getVerse(
       return;
     }
 
-    res.status(200).json({ data: `${resultVerse}` });
+    res.status(200).json({ data: `${verse}` });
   } catch (err) {
     console.error(err);
     next(err);
