07f274ddd14dc0ad2b47dc0d8d9404f6b72ac6ee
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-10-30 10:46:21 +0000

    Adding a bunch of comments

--- src/backend/controllers/bibleVerseController.ts
+++ src/backend/controllers/bibleVerseController.ts
@@ -10,23 +10,29 @@ async function getVerse(
   next: Function
 ): Promise<void> {
   try {
+    // Creating a function which will return a string response from the url
     const getString: bent.RequestFunction<string> = await bent("string");
-    // `https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/${req.params.bookNumber}/1#study=discover&v=${req.params.bookNumber}:${req.params.chapter}:${req.params.verse}`
 
+    // The string response from the website
     const responseString: string = await getString(
       `https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/${req.params.bookNumber}/${req.params.chapter}#study=discover&v=${req.params.bookNumber}:${req.params.chapter}:${req.params.verse}`
     );
 
-    const data: HTMLElement = parse(responseString);
+    // id for the html element
     const idString: string = `v${req.params.bookNumber}-${req.params.chapter}-${req.params.verse}-1`;
     console.log(idString);
 
+    // converting data into manipulatable html
+    const data: HTMLElement = parse(responseString);
+
+    // extracting verse text with the id
     const verse: string = data
       .getElementById(idString)
       // Remove non-alpha-whitespace characters e.g. <p>hello</p> => phellop
       .text.replace(/[0-9+*]/g, "")
       .trim();
 
+    // checking if the verse was extracted
     if (!verse) {
       res.status(400).json({
         error: "Invalid entry, please check your request and retry.",
@@ -38,6 +44,7 @@ async function getVerse(
     // sending response to the user
     res.status(200).json({ data: verse });
   } catch (err) {
+    // logging the error and sending error to next middleware
     console.error(err);
     next(err);
   }
