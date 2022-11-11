da087015e97fa8dcbb665d86a5c8558142008b20
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-11-11 20:20:27 +0000

    salvaging what was left from the aftermath of git merging

--- src/backend/controllers/bibleVerseController.ts
+++ src/backend/controllers/bibleVerseController.ts
@@ -1,7 +1,6 @@
-import request from "request";
-import { parse } from "node-html-parser";
 import { Request, Response } from "express";
-import console from "console";
+
+import getHTML from "../utils/getHTMLData";
 
 async function getVerse(
   req: Request,
@@ -9,53 +8,33 @@ async function getVerse(
   next: Function
 ): Promise<void> {
   try {
-    request(
-      `https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/${req.params.bookNumber}/1#study=discover&v=${req.params.bookNumber}:${req.params.chapter}:${req.params.verse}`,
-      (err: Error, response: any, body: string) => {
-        if (err) {
-          console.error(err);
-          res
-            .status(500)
-            .json({ error: "Server-side error, aborted request." });
+    const data = await getHTML(
+      `https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/${req.params.bookNumber}/${req.params.chapter}#study=discover&v=${req.params.bookNumber}:${req.params.chapter}:${req.params.verse}`
+    );
 
     // id for the html element
     const idString: string = `v${req.params.bookNumber}-${req.params.chapter}-${req.params.verse}-1`;
     console.log(idString);
 
-        const data = parse(body);
-        const verseNumber = parseInt(req.params.verse);
-        const idString = `p${verseNumber + 1}`;
+    // getting the verse text
+    const verse = data
+      .getElementById(idString)
+      // Remove non-alpha-whitespace characters e.g. <p>hello</p> => phellop
+      .text.replace(/[0-9+*]/g, "")
+      .trim();
 
-        const verse = data
-          // getting all the elements with the "p" tag
-          .getElementsByTagName("p")
-          .filter(
-            // matching the id to the right element
-            (val) => val.id === idString && val.rawAttrs.includes(`class="sb"`)
-          )
-          // Map returns an array
-          .map((val) => {
-            // Remove non-alpha-whitespace characters e.g. <p>hello</p> => phellop
-            const outputStr = val.text.replace(/[^a-zA-Z-.\s]/g, "").trim();
-            return outputStr;
-          });
+    // sending response to the user
+    const resultVerse = verse[0];
 
-        // sending response to the user
-        const resultVerse = verse[0];
+    if (!resultVerse) {
+      res.status(400).json({
+        error: "Invalid entry, please check your request and retry.",
+      });
+      next(Error("Result is undefined!"));
+      return;
+    }
 
-        if (!resultVerse) {
-          res
-            .status(400)
-            .json({
-              error: "Invalid entry, please check your request and retry.",
-            });
-          next(Error("Result is undefined!"));
-          return;
-        }
-
-        res.status(200).json({ data: `${resultVerse}` });
-      }
-    );
+    res.status(200).json({ data: `${resultVerse}` });
   } catch (err) {
     // logging the error and sending error to next middleware
     console.error(err);
