2ba366d2883a2e5ff5c51146e0e4bfede08fa333
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-11-13 08:53:46 +0000

    Creating base user model #15

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-dotenv.config({ path: "./env" });
+dotenv.config();
 
 import morgan from "morgan";
 
@@ -13,6 +13,7 @@ import xss from "xss";
 // When making local imports its important to add the .js extension because node is not smart enough to figure it out by itself
 import bibleVerseRouter from "./routes/bibleVerseRoutes.js";
 import { homeController } from "./controllers/homeController.js";
+import process from "process";
 
 // creating express app
 const app = express();
@@ -38,10 +39,12 @@ const app = express();
 
 // logging software for development
 // logs http method, time it took to make request etc
+console.log("The node environment is... ", process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 // This is where we mount the routes
 
+// Routes which devs using the api are going to be physically interacting with.
 app.use("/home", homeController);
 
 app.use("/api/v1/", bibleVerseRouter);
---
+++ src/backend/models/userModel.ts
@@ -0,0 +1,29 @@
+import { Schema, Model } from "mongoose";
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    maxLength: 20,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    maxLenth: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    // Needs to be validated
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+    // Needs to be encrypted
+  },
+});
+
+const User = new Model(userSchema, "user");
+
+export default User;
