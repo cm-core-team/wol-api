e914f4691ef3cf69e1ac4c39082f6dee93eb2ad6
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-11-22 13:51:41 +0000

    Creating a nav bar for the front end

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Verse from "./components/Verse/Verse";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Verse />
     </div>
   );
---
+++ src/frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="navBar">
+      <button className="home-btn">home</button>
+      <button className="about-btn">about</button>
+      <button className="gh-btn"></button>
+    </div>
+  );
+}
+
+export default NavBar;
