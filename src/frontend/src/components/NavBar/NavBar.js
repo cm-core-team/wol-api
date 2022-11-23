e8b851d06fde0adfc8459b2e1515e201b038ffab
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-11-23 17:26:55 +0000

    Humble beginnings of my nav bar
    react bootstrap is actually awesome

--- src/frontend/src/components/NavBar/NavBar.js
+++ src/frontend/src/components/NavBar/NavBar.js
@@ -1,12 +1,32 @@
+// styling
 import "./NavBar.css";
+
+// bootstrap
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function NavBar() {
   return (
-    <div className="navBar">
-      <button className="home-btn">home</button>
-      <button className="about-btn">about</button>
-      <button className="gh-btn"></button>
-    </div>
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">wol-api</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link
+              href="https://github.com/j-koziel/wol-api"
+              target="_blank"
+              className="gh-lnk"
+            >
+              
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
