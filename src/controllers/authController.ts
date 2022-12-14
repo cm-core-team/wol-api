bea63c0ee0c354985765c2d1ab59f3fe180f1d2f
Jonathan Koziel (40184214) <40184214@hcuc.ac.uk>
2022-12-14 15:07:56 +0000

    Beginning to write JSDoc comments.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,56 +1,70 @@
 import { Request, Response, NextFunction } from "express";
 
 // Protect middleware to protect routes from unauthorized requests from the wrong type of user.
+/**
+ * Middleware to protect routes from unauthorized requests.
+ *
+ * @param req
+ * @param res
+ * @param next
+ * @param restrictTo
+ */
 async function protect(
-    req: Request,
-    res: Response,
-    next: NextFunction
-    // eslint-disable-next-line capitalized-comments
-    // restrictTo: string
+  req: Request,
+  res: Response,
+  next: NextFunction
+  // eslint-disable-next-line capitalized-comments
+  // restrictTo: string
 ): Promise<void> {
-    try {
-        /*
-         *
-         * Route protection （￣︶￣）↗
-         *
-         */
-    } catch (err) {
-        next(err);
-    }
+  try {
+    /*
+     *
+     * Route protection （￣︶￣）↗
+     *
+     */
+  } catch (err) {
+    next(err);
+  }
 }
 
-// Middleware to log users in if their credentials are correct.
+/**
+ * Middleware to log users in.
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
 async function login(
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> {
-    try {
-        /**
-         *
-         * Do authentication stuff
-         *
-         */
-    } catch (err) {
-        next(err);
-    }
+  try {
+    /**
+     *
+     * Do authentication stuff
+     *
+     */
+  } catch (err) {
+    next(err);
+  }
 }
 
 // Middleware to sign users up and log them in.
 async function signup(
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> {
-    try {
-        /**
-         *
-         * Do more authentication stuff
-         *
-         */
-    } catch (err) {
-        next(err);
-    }
+  try {
+    /**
+     *
+     * Do more authentication stuff
+     *
+     */
+  } catch (err) {
+    next(err);
+  }
 }
 
 export { protect, login, signup };
