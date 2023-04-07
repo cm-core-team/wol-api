ad4c8174be793ab2de757d34e5cac20ce5178b68
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-04-07 13:00:49 +0100

    feat(users): removing db connection routines

--- src/server.ts
+++ src/server.ts
@@ -16,18 +16,7 @@ process.on("uncaughtException", (err: Error) => {
   process.exit(1);
 });
 
-// The port the server will run on
 const PORT: string | undefined = process.env.PORT;
-
-// As of mongoose 7 this is going to be set to "false" by default
-mongoose.set("strictQuery", true);
-
-// Connect to mongo db
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.vmbese9.mongodb.net/?retryWrites=true&w=majority`
-  )
-  .then((): void => console.log("Database has been connected."));
 
 // Starting the server
 const server = app.listen(PORT, (): void => {
