a1eb5b29c10af5980c8b844ccaa99316ba532e2b
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-04-16 20:02:24 +0100

    ops: creating spring boot project

---
+++ src/test/java/com/wolApi/wolApi/WolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolApi.wolApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
