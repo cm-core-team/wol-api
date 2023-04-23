6bde5365bb292b45c4653c9ab38b4864f3d7e968
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-04-23 21:49:48 +0100

    feat(java mig): using Jsoup to parse HTML

--- src/main/java/com/wolApi/wolApi/BibleVerseController.java
+++ src/main/java/com/wolApi/wolApi/BibleVerseController.java
@@ -1,21 +1,24 @@
 package com.wolApi.wolApi;
 
+import org.jsoup.Connection;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.net.ssl.HttpsURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
+import java.io.IOException;
+
 
 @RestController
 @RequestMapping("/api/v1/bible-verses")
 public class BibleVerseController {
     @GetMapping("/get-verse")
-    public Verse getVerse() {
-        URL url = new URL("https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/1/1#study=discover&v=1:1:1");
-        HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
+    public Verse getVerse() throws IOException {
+        Document doc = Jsoup.connect("https://wol.jw.org/en/wol/b/r1/lp-e/nwtsty/1/1#study=discover&v=1:1:1")
+                .get();
+        doc.select("p").forEach(System.out::println);
 
         return new Verse("Welcome to the java port of this app");
     }
