881857188ab24bfb5c9a60644ccafc0a9a9619d9
Jude Davis <judedavis92@gmail.com>
2023-05-15 08:27:11 +0100

    refactor: verse utils

--- src/main/java/com/wolApi/wolApi/utils/VerseUtils.java
+++ src/main/java/com/wolApi/wolApi/utils/VerseUtils.java
@@ -10,17 +10,36 @@ import java.io.IOException;
 import java.util.LinkedList;
 
 public class VerseUtils {
+    /**
+     * Get a specific verses given book, chapter, and verse
+     *
+     * @param book
+     * @param chapter
+     * @param verse
+     * @return Single verse text
+     * @throws IOException
+     */
     public static String getVerse(String book, String chapter, String verse) throws IOException {
-        Document doc = Jsoup.connect(AppSettings.getInstance().mainVerseURL(book, chapter)).get();
-        String verseIdString = String.format("v%s-%s-%s-1", book, chapter, verse);
+        Document doc = Jsoup.connect(
+                AppSettings.getInstance().mainVerseURL(book, chapter)
+        ).get();
+        String verseIdString = AppSettings.getInstance().getVerseID(book, chapter, verse);
 
         return doc.select("#" + verseIdString)
-                .text().
-                replaceAll("[0-9+*]", "")
+                .text()
+                .replaceAll("[0-9+*]", "")
                 .stripLeading()
                 .stripTrailing();
     }
 
+    /**
+     * Gets the verses in a chapter in a list.
+     *
+     * @param book
+     * @param chapter
+     * @return Verses in the chapter
+     * @throws IOException
+     */
     public static LinkedList<String> getVersesInChapter(
             String book,
             String chapter) throws IOException {
@@ -31,8 +50,7 @@ public class VerseUtils {
 
         LinkedList<String> verses = new LinkedList<>();
         for (Element element : doc.select(".v")) {
-            String verse = element
-                    .text()
+            String verse = element.text()
                     // Remove extraneous HTML syntax
                     .replaceAll("[0-9+*]", "")
 
