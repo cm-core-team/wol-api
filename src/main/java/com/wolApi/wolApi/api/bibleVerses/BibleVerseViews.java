1d30fd325f05f1e1d5284d1b6f68b5cac111eb94
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-05-21 20:30:44 +0100

    refactor(#210): using request params instead of path variables

--- src/main/java/com/wolApi/wolApi/api/bibleVerses/BibleVerseViews.java
+++ src/main/java/com/wolApi/wolApi/api/bibleVerses/BibleVerseViews.java
@@ -4,10 +4,7 @@ import com.wolApi.wolApi.api.bibleVerses.dtos.Verse;
 import com.wolApi.wolApi.api.bibleVerses.dtos.VerseList;
 import com.wolApi.wolApi.api.bibleVerses.services.BibleVerseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -21,10 +18,10 @@ public class BibleVerseViews {
     public BibleVerseViews(BibleVerseService bibleVerseService) {
         this.bibleVerseService = bibleVerseService;
     }
-    @GetMapping("/get-verse/{bookNum}/{chapterNum}/{verseNum}")
-    public Verse getVerse(@PathVariable("bookNum") String bookNum,
-                          @PathVariable("chapterNum") String chapterNum,
-                          @PathVariable("verseNum") String verseNum) throws IOException, InterruptedException {
+    @GetMapping("/get-verse")
+    public Verse getVerse(@RequestParam String bookNum,
+                          @RequestParam String chapterNum,
+                          @RequestParam String verseNum) throws IOException, InterruptedException {
         return new Verse(
                 bibleVerseService.getVerse(bookNum, chapterNum, verseNum),
                 bibleVerseService.getNumVersesInChapter(chapterNum),
@@ -33,9 +30,9 @@ public class BibleVerseViews {
         );
     }
 
-    @GetMapping("/get-verse/{bookNum}/{chapterNum}")
-    public VerseList getVerses(@PathVariable("bookNum") String bookNum,
-                               @PathVariable("chapterNum") String chapterNum) throws IOException {
+    @GetMapping("/get-verses")
+    public VerseList getVerses(@RequestParam String bookNum,
+                               @RequestParam String chapterNum) throws IOException {
         return new VerseList(
                 bibleVerseService.getVersesInChapter(bookNum, chapterNum)
         );
