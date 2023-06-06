0ffc0c05e902c873d1e66adebda2ff1c4b966397
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-06-06 16:44:37 +0100

    chore(#212): adding back commented code
    just in case something breaks

--- src/main/java/com/wolApi/wolApi/api/bibleVerses/services/BibleVerseService.java
+++ src/main/java/com/wolApi/wolApi/api/bibleVerses/services/BibleVerseService.java
@@ -32,4 +32,77 @@ public class BibleVerseService {
         return bibleVerseRepository.findAllVersesInChapter(bookNum, chapter);
     }
 
+    ///////////////////////////////////////////////////////////////////////////
+    // Code which might be reused later
+    ///////////////////////////////////////////////////////////////////////////
+    //    private JsonNode jsonForExtraVerseInfo;
+//
+//    private String extractVerseFromHtml(String bookNum, String chapterNum, String verseNum) throws IOException {
+//        Document doc = Jsoup.connect(
+//                AppSettings.getInstance().mainVerseURL(bookNum, chapterNum)
+//        ).get();
+//        String verseIdString = constructVerseId(bookNum, chapterNum, verseNum);
+//
+//        return doc.select("#" + verseIdString)
+//                .text()
+//                .replaceAll("[0-9+*]", "")
+//                .stripLeading()
+//                .stripTrailing();
+//    }
+
+//    private JsonNode getJsonDataForExtraVerseInfo(String bookNum) throws IOException, InterruptedException {
+//        URI uri = URI.create(String
+//                .format("https://b.jw-cdn.org/apis/pub-media/GETPUBMEDIALINKS?pub=nwt&langwritten=E&txtCMSLang=E&fileformat=mp3&booknum=%s",
+//                        bookNum));
+//
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(uri)
+//                .build();
+//        String json = HttpClient.newHttpClient()
+//                .send(request, HttpResponse.BodyHandlers.ofString())
+//                .body();
+//        ObjectMapper mapper = new ObjectMapper();
+//        return mapper.readTree(json);
+//    }
+
+
+//    private String constructVerseId(String bookNum, String chapterNum, String verseNum) {
+//        return String.format("v%s-%s-%s-1", bookNum, chapterNum, verseNum);
+//    }
+
+    //    public int getNumVersesInChapter(String chapterNum) {
+//        JsonNode json = this.jsonForExtraVerseInfo;
+//        int numVerses = json
+//                .get("files")
+//                .get("E")
+//                .get("MP3")
+//                .get(Integer.parseInt(chapterNum) - 1)
+//                .get("markers")
+//                .get("markers")
+//                .size();
+//
+//        return numVerses;
+//    }
+
+
+
+//    public int getNumChaptersInBook() {
+//        JsonNode json = this.jsonForExtraVerseInfo;
+//        int numChapters = json
+//                .get("files")
+//                .get("E")
+//                .get("MP3")
+//                .size();
+//
+//        return numChapters;
+//    }
+
+//    public String getBookName() {
+//        JsonNode json = this.jsonForExtraVerseInfo;
+//        String bookName = json
+//                        .get("pubName")
+//                        .asText();
+//
+//        return bookName;
+//    }
 }
