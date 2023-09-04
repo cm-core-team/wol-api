f8b978fc257ab3085f56b5f5685dedf053ce573e
j-koziel <jonathankoziel1@gmail.com>
2023-09-04 20:54:19 +0100

    feat(#220): first route woohooo!!!

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,42 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::serde::{json::Json, Serialize};
+
+// for now
 #[get("/")]
-fn index() -> &'static str {
+fn home() -> &'static str {
     "Hello, world!"
+}
+
+// idk rust well enough to properly refactor this
+// lemme cook
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct BibleVerses {
+    num_verses: u32,
+    book: (u32, &'static str),
+    chapter: u32,
+    data: Vec<String>,
+}
+
+#[get("/bible-verses")]
+fn bible_verses() -> Json<BibleVerses> {
+    let example_bible_verses = BibleVerses {
+        num_verses: 1,
+        book: (1, "Genesis"),
+        chapter: 1,
+        data: vec![String::from(
+            "In the beginning God created the heavens and the earth.",
+        )],
+    };
+
+    Json(example_bible_verses)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![home])
+        .mount("/api/v1/", routes![bible_verses])
 }
