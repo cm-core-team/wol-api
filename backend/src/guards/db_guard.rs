f5b9e8850016d40bd99b538c0527d679ca353dd6
Jude Davis <judedavis92@gmail.com>
2023-09-17 21:04:21 +0100

    feat: working get verse endpoint

--- backend/src/guards/db_guard.rs
+++ backend/src/guards/db_guard.rs
@@ -3,8 +3,13 @@ use rocket::{
     request::{FromRequest, Outcome},
 };
 
+use sqlx;
+
+#[derive(Debug)]
+pub struct DbGuardError;
+
 pub struct DbGuard<'r> {
-    pool: &'r sqlx::PgPool,
+    pub pool: &'r sqlx::PgPool,
 }
 
 #[rocket::async_trait]
@@ -16,9 +21,6 @@ impl<'r> FromRequest<'r> for DbGuard<'r> {
     ) -> rocket::request::Outcome<Self, Self::Error> {
         // Extract the database pool from the request's managed state
         let pool = request.rocket().state::<sqlx::PgPool>().unwrap();
-
-        // Here, you can add any additional logic or checks related to the database
-
         Outcome::Success(DbGuard { pool })
     }
 }
--- backend/src/services/verse_services.rs
+++ backend/src/services/verse_services.rs
@@ -8,12 +8,14 @@ pub async fn get_verse_from_table(
     verse: i32,
     pool: &sqlx::PgPool,
 ) -> Result<BibleVerse, sqlx::Error> {
-    let verse: BibleVerse = sqlx::query_as("SELECT * FROM bible_verses WHERE book = $1 AND chapter = $2 AND verse = $3")
-        .bind(book)
-        .bind(chapter)
-        .bind(verse)
-        .fetch_one(pool)
-        .await?;
+    let verse: BibleVerse = sqlx::query_as(
+        "SELECT * FROM verses WHERE book_num = $1 AND chapter = $2 AND verse_num = $3",
+    )
+    .bind(book)
+    .bind(chapter)
+    .bind(verse)
+    .fetch_one(pool)
+    .await?;
 
     return Ok(verse);
 }
